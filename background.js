f5e5836dc1cde736d44c57355b353e920e9a9fe9
Dan <danhcas@gmail.com>
2016-08-09 23:13:43 -0400

    Doot doot

--- background.js
+++ background.js
@@ -73,8 +73,8 @@ function deactivateProductionDangerOnTab(tab){
   setBlackIcon(tab);
 }
 
-var BLACK_ICON_PATH = 'icon-toxic@2x.png';
-var RED_ICON_PATH = 'icon-toxic--active@2x.png';
+var BLACK_ICON_PATH = 'icon-skull@2x.png';
+var RED_ICON_PATH = 'icon-skull--active@2x.png';
 
 function setBlackIcon(tab){
   setIcon(BLACK_ICON_PATH, tab);
